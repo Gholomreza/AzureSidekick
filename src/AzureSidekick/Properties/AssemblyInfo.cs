4862264aeda6d76e1ab1c0ead5b841722e2c89f5
Gholomreza <Gholomreza@gmail.com>
2025-08-17 13:42:03 +0330

    Attrice Corporation

--- src/AzureSidekick/Properties/AssemblyInfo.cs
+++ src/AzureSidekick/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("AzureSidekick")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Attrice Corporation")]
 [assembly: AssemblyProduct("AzureSidekick")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
